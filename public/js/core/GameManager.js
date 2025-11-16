419a3dcea8e99f7270e189baeb18b1daaf6b22a5
木村好孝 <kim04190659@gmail.com>
2025-11-16 14:34:14 +0900

    [UPDATE] GameManager.js にロボットゲームを追加

--- public/js/core/GameManager.js
+++ public/js/core/GameManager.js
@@ -23,6 +23,7 @@ class GameManager {
       // 利用可能なゲームのリスト
       const gameList = [
         { id: 'city-dx', name: '自治体DX推進ゲーム', file: 'city-dx.json' },
+        { id: 'robot-solution', name: 'ロボットソリューション創出ゲーム', file: 'robot-solution.json' },
         // 将来追加予定
         // { id: 'it-operations', name: 'IT運用エクセレンス', file: 'it-operations.json' },
         // { id: 'sales-support', name: '営業支援ソリューション', file: 'sales-support.json' }
